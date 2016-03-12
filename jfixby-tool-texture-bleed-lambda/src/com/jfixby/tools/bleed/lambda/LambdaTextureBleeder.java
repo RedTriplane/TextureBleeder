d8456eea0b969a8d669d673acfcb357fa50ea2b5
J Fixby <github@jfixby.com>
2016-03-12 21:58:57 +0100

    ...

--- jfixby-tool-texture-bleed-lambda/src/com/jfixby/tools/bleed/lambda/LambdaTextureBleeder.java
+++ jfixby-tool-texture-bleed-lambda/src/com/jfixby/tools/bleed/lambda/LambdaTextureBleeder.java
@@ -8,183 +8,184 @@ import com.jfixby.cmns.api.collections.List;
 import com.jfixby.cmns.api.color.Color;
 import com.jfixby.cmns.api.color.Colors;
 import com.jfixby.cmns.api.color.CustomColor;
+import com.jfixby.cmns.api.desktop.ImageAWT;
 import com.jfixby.cmns.api.file.ChildrenList;
 import com.jfixby.cmns.api.file.File;
 import com.jfixby.cmns.api.image.ColorMap;
+import com.jfixby.cmns.api.image.ColorMapSpecs;
+import com.jfixby.cmns.api.image.ColoredλImage;
 import com.jfixby.cmns.api.image.ImageProcessing;
-import com.jfixby.cmns.api.image.LambdaColorMapSpecs;
 import com.jfixby.cmns.api.lambda.Lambda;
 import com.jfixby.cmns.api.lambda.λFunction;
-import com.jfixby.cmns.api.lambda.img.λImage;
-import com.jfixby.cv.api.cv.CV;
-import com.jfixby.cv.api.cv.λOperator;
-import com.jfixby.cv.api.gwt.ImageGWT;
+import com.jfixby.cv.api.CV;
+import com.jfixby.cv.api.λOperator;
 import com.jfixby.tools.bleed.api.TextureBleedComponent;
 import com.jfixby.tools.bleed.api.TextureBleedResult;
 import com.jfixby.tools.bleed.api.TextureBleedSpecs;
 
 public class LambdaTextureBleeder implements TextureBleedComponent {
 
-	private int W;
-	private int H;
-	private int maxScans;
-	private λImage emty = (x, y) -> Colors.PURPLE();
-	private λFunction<Integer, λImage> STACK;
+    private int W;
+    private int H;
+    private int maxScans;
+    private ColoredλImage emty = (x, y) -> Colors.PURPLE();
+    private λFunction<Integer, ColoredλImage> STACK;
 
-	@Override
-	public TextureBleedSpecs newTextureBleedSpecs() {
-		// TODO Auto-generated method stub
-		return new TextureBleedSpecsImpl();
+    @Override
+    public TextureBleedSpecs newTextureBleedSpecs() {
+	// TODO Auto-generated method stub
+	return new TextureBleedSpecsImpl();
+    }
+
+    @Override
+    public TextureBleedResult process(TextureBleedSpecs specs) throws IOException {
+	TextureBleedResultImpl result = new TextureBleedResultImpl();
+
+	File folder = specs.getInputFolder();
+	ChildrenList pngFiles = folder.listChildren().filter(n -> {
+	    return n.getName().toLowerCase().endsWith(".png");
+	});
+
+	boolean debug_mode = specs.getDebugMode();
+	if (debug_mode) {
+	    this.ALPHA = this.DEBUG_MODE_ALPHA;
 	}
 
-	@Override
-	public TextureBleedResult process(TextureBleedSpecs specs) throws IOException {
-		TextureBleedResultImpl result = new TextureBleedResultImpl();
+	maxScans = specs.getPaddingSize();
+	if (maxScans < 0) {
+	    maxScans = TextureBleedSpecs.DEFAULT_PADDING;
+	}
 
-		File folder = specs.getInputFolder();
-		ChildrenList pngFiles = folder.listChildren().filter(n -> {
-			return n.getName().toLowerCase().endsWith(".png");
-		});
+	System.out.println("maxScans: " + maxScans);
 
-		boolean debug_mode = specs.getDebugMode();
-		if (debug_mode) {
-			this.ALPHA = this.DEBUG_MODE_ALPHA;
+	for (File png : pngFiles) {
+	    process(png, result);
+	}
+
+	return result;
+
+    }
+
+    public static final λOperator wrap = null;
+
+    private void process(File png, TextureBleedResultImpl process_result) throws IOException {
+	FileResultImpl fileResult = new FileResultImpl();
+	fileResult.setProcessedFile(png);
+	long start_time = System.currentTimeMillis();
+	process_result.addFileResult(fileResult);
+	System.out.println("Processing: " + png);
+	BufferedImage image = ImageAWT.readFromFile(png);
+	ColorMap img = ImageAWT.newAWTColorMap(image);
+	W = img.getWidth();
+	H = img.getHeight();
+
+	ColoredλImage λimage = img.getLambdaImage();
+	λFunction<Integer, ColoredλImage> layers_stack = buildStack(maxScans, λimage, W, H);
+	STACK = layers_stack;
+
+	ColoredλImage result = merge(layers_stack, W, H, maxScans);
+
+	// λImage result = wrap(maxScans, λimage, W, H);
+	int k = 0;
+	System.out.println();
+	// System.out.println("Scans performed: " + k);
+	fileResult.setScansPerformed(k);
+	// int maxDistance = colors.size();
+	ColorMapSpecs lambda_specs = ImageProcessing.newLambdaColorMapSpecs();
+	lambda_specs.setColorMapWidth(W);
+	lambda_specs.setColorMapHeight(H);
+	lambda_specs.setLambdaColoredImage(result);
+	img = ImageProcessing.newColorMap(lambda_specs);
+	image = ImageAWT.toAWTImage(img);
+	ImageAWT.writeToFile(image, png, "png");
+	long mills = System.currentTimeMillis() - start_time;
+	fileResult.setDoneInMills(mills);
+    }
+
+    private ColoredλImage merge(λFunction<Integer, ColoredλImage> layers_stack, int w, int h, int maxScans) {
+	ColoredλImage result = (x, y) -> {
+
+	    for (int i = 0; i < maxScans; i++) {
+		ColoredλImage fn = layers_stack.val(i);
+		Color color = fn.valueAt(x, y);
+		if (color.alpha() == 1) {
+		    return color;
+		}
+	    }
+	    return Colors.PURPLE();
+	};
+	return result;
+    }
+
+    private λFunction<Integer, ColoredλImage> buildStack(int maxScans, ColoredλImage λimage, int w, int h) {
+	λFunction<Integer, ColoredλImage> stack = i -> {
+	    if (i == 0) {
+		return λimage;
+	    }
+	    if (i >= maxScans) {
+		return emty;
+	    }
+	    return fn(i, STACK, w, h);
+
+	};
+	stack = Lambda.cache(stack);
+	return stack;
+    }
+
+    private ColoredλImage fn(int n, λFunction<Integer, ColoredλImage> STACK, int w, int h) {
+	ColoredλImage fn = (x, y) -> {
+	    ColoredλImage fn_1 = STACK.val(n - 1);
+	    Color color = fn_1.valueAt(x, y);
+	    if (color.alpha() == 1) {
+		return color;
+	    }
+	    List<Color> collectedColors = Collections.newList();
+	    collectNotNullNeighbours((long) x, (long) y, fn_1, w, h, collectedColors);
+	    if (collectedColors.size() > 0) {
+		CustomColor average = Colors.newColor();
+		CV.averageColor(collectedColors, average);
+		// average = Colors.BLACK().customize();
+		return average;
+	    }
+	    return Colors.NO();
+	};
+	return CV.cache(fn, ImageProcessing.newImageCache(w, h));
+	// return fn;
+    }
+
+    private void collectNotNullNeighbours(long x0, long y0, ColoredλImage λimage, int W, int H,
+	    List<Color> collectedColors) {
+	int D = 1;
+	for (int k = -D; k <= D; k++) {
+	    for (int p = -D; p <= D; p++) {
+		if (k == 0 && p == 0) {
+		    continue;
+		}
+		int x = (int) (k + x0);
+		int y = (int) (p + y0);
+		if (x < 0) {
+		    continue;
+		}
+		if (y < 0) {
+		    continue;
+		}
+		if (x >= W) {
+		    continue;
+		}
+		if (y >= H) {
+		    continue;
 		}
 
-		maxScans = specs.getPaddingSize();
-		if (maxScans < 0) {
-			maxScans = TextureBleedSpecs.DEFAULT_PADDING;
+		Color neighbour_color = λimage.valueAt(x, y);
+		if (neighbour_color.alpha() == 1f) {
+		    collectedColors.add(neighbour_color);
 		}
-
-		System.out.println("maxScans: " + maxScans);
-
-		for (File png : pngFiles) {
-			process(png, result);
-		}
-
-		return result;
-
+	    }
 	}
+    }
 
-	public static final λOperator wrap = null;
-
-	private void process(File png, TextureBleedResultImpl process_result) throws IOException {
-		FileResultImpl fileResult = new FileResultImpl();
-		fileResult.setProcessedFile(png);
-		long start_time = System.currentTimeMillis();
-		process_result.addFileResult(fileResult);
-		System.out.println("Processing: " + png);
-		BufferedImage image = ImageGWT.readFromFile(png);
-		ColorMap img = ImageGWT.newGWTColorMap(image);
-		W = img.getWidth();
-		H = img.getHeight();
-
-		λImage λimage = img.getLambdaImage();
-		λFunction<Integer, λImage> layers_stack = buildStack(maxScans, λimage, W, H);
-		STACK = layers_stack;
-
-		λImage result = merge(layers_stack, W, H, maxScans);
-
-		// λImage result = wrap(maxScans, λimage, W, H);
-		int k = 0;
-		System.out.println();
-		// System.out.println("Scans performed: " + k);
-		fileResult.setScansPerformed(k);
-		// int maxDistance = colors.size();
-		LambdaColorMapSpecs lambda_specs = ImageProcessing.newLambdaColorMapSpecs();
-		lambda_specs.setColorMapWidth(W);
-		lambda_specs.setColorMapHeight(H);
-		lambda_specs.setLambdaColoredImage(result);
-		img = ImageProcessing.newLambdaColorMap(lambda_specs);
-		image = ImageGWT.toGWTImage(img);
-		ImageGWT.writeToFile(image, png, "png");
-		long mills = System.currentTimeMillis() - start_time;
-		fileResult.setDoneInMills(mills);
-	}
-
-	private λImage merge(λFunction<Integer, λImage> layers_stack, int w, int h, int maxScans) {
-		λImage result = (x, y) -> {
-
-			for (int i = 0; i < maxScans; i++) {
-				λImage fn = layers_stack.val(i);
-				Color color = fn.valueAt(x, y);
-				if (color.alpha() == 1) {
-					return color;
-				}
-			}
-			return Colors.PURPLE();
-		};
-		return result;
-	}
-
-	private λFunction<Integer, λImage> buildStack(int maxScans, λImage λimage, int w, int h) {
-		λFunction<Integer, λImage> stack = i -> {
-			if (i == 0) {
-				return λimage;
-			}
-			if (i >= maxScans) {
-				return emty;
-			}
-			return fn(i, STACK, w, h);
-
-		};
-		stack = Lambda.cache(stack);
-		return stack;
-	}
-
-	private λImage fn(int n, λFunction<Integer, λImage> STACK, int w, int h) {
-		λImage fn = (x, y) -> {
-			λImage fn_1 = STACK.val(n - 1);
-			Color color = fn_1.valueAt(x, y);
-			if (color.alpha() == 1) {
-				return color;
-			}
-			List<Color> collectedColors = Collections.newList();
-			collectNotNullNeighbours((long) x, (long) y, fn_1, w, h, collectedColors);
-			if (collectedColors.size() > 0) {
-				CustomColor average = Colors.newColor();
-				CV.averageColor(collectedColors, average);
-				// average = Colors.BLACK().customize();
-				return average;
-			}
-			return Colors.NO();
-		};
-		return CV.cache(fn, CV.newImageCache(w, h));
-		// return fn;
-	}
-
-	private void collectNotNullNeighbours(long x0, long y0, λImage λimage, int W, int H, List<Color> collectedColors) {
-		int D = 1;
-		for (int k = -D; k <= D; k++) {
-			for (int p = -D; p <= D; p++) {
-				if (k == 0 && p == 0) {
-					continue;
-				}
-				int x = (int) (k + x0);
-				int y = (int) (p + y0);
-				if (x < 0) {
-					continue;
-				}
-				if (y < 0) {
-					continue;
-				}
-				if (x >= W) {
-					continue;
-				}
-				if (y >= H) {
-					continue;
-				}
-
-				Color neighbour_color = λimage.valueAt(x, y);
-				if (neighbour_color.alpha() == 1f) {
-					collectedColors.add(neighbour_color);
-				}
-			}
-		}
-	}
-
-	float NORMAL_MODE_ALPHA = 0f;
-	float DEBUG_MODE_ALPHA = 1f;
-	float ALPHA = NORMAL_MODE_ALPHA;
+    float NORMAL_MODE_ALPHA = 0f;
+    float DEBUG_MODE_ALPHA = 1f;
+    float ALPHA = NORMAL_MODE_ALPHA;
 
 }
--- rebecca-tool-texture-bleed/src/com/github/wrebecca/bleed/RebeccaTextureBleeder.java
+++ rebecca-tool-texture-bleed/src/com/github/wrebecca/bleed/RebeccaTextureBleeder.java
@@ -6,13 +6,13 @@ import java.util.HashSet;
 
 import com.jfixby.cmns.api.color.Color;
 import com.jfixby.cmns.api.color.Colors;
+import com.jfixby.cmns.api.desktop.ImageAWT;
 import com.jfixby.cmns.api.file.ChildrenList;
 import com.jfixby.cmns.api.file.File;
 import com.jfixby.cmns.api.image.EditableColorMap;
 import com.jfixby.cmns.api.math.Int2;
 import com.jfixby.cmns.api.math.IntegerMath;
 import com.jfixby.cmns.api.sys.Sys;
-import com.jfixby.cv.api.gwt.ImageGWT;
 import com.jfixby.tools.bleed.api.TextureBleedComponent;
 import com.jfixby.tools.bleed.api.TextureBleedResult;
 import com.jfixby.tools.bleed.api.TextureBleedSpecs;
@@ -51,216 +51,216 @@ import com.jfixby.tools.bleed.api.TextureBleedSpecs;
 
 public class RebeccaTextureBleeder implements TextureBleedComponent {
 
-	private int W;
-	private int H;
-	private int maxScans;
+    private int W;
+    private int H;
+    private int maxScans;
 
-	@Override
-	public TextureBleedSpecs newTextureBleedSpecs() {
-		// TODO Auto-generated method stub
-		return new TextureBleedSpecsImpl();
+    @Override
+    public TextureBleedSpecs newTextureBleedSpecs() {
+	// TODO Auto-generated method stub
+	return new TextureBleedSpecsImpl();
+    }
+
+    @Override
+    public TextureBleedResult process(TextureBleedSpecs specs) throws IOException {
+	TextureBleedResultImpl result = new TextureBleedResultImpl();
+
+	File folder = specs.getInputFolder();
+	ChildrenList pngFiles = folder.listChildren().filter(n -> {
+	    return n.getName().toLowerCase().endsWith(".png");
+	});
+
+	boolean debug_mode = specs.getDebugMode();
+	if (debug_mode) {
+	    this.ALPHA = this.DEBUG_MODE_ALPHA;
 	}
 
-	@Override
-	public TextureBleedResult process(TextureBleedSpecs specs) throws IOException {
-		TextureBleedResultImpl result = new TextureBleedResultImpl();
-
-		File folder = specs.getInputFolder();
-		ChildrenList pngFiles = folder.listChildren().filter(n -> {
-			return n.getName().toLowerCase().endsWith(".png");
-		});
-
-		boolean debug_mode = specs.getDebugMode();
-		if (debug_mode) {
-			this.ALPHA = this.DEBUG_MODE_ALPHA;
-		}
-
-		maxScans = specs.getPaddingSize();
-		if (maxScans < 0) {
-			maxScans = TextureBleedSpecs.DEFAULT_PADDING;
-		}
-
-		System.out.println("maxScans: " + maxScans);
-
-		for (File png : pngFiles) {
-			process(png, result);
-		}
-
-		return result;
-
+	maxScans = specs.getPaddingSize();
+	if (maxScans < 0) {
+	    maxScans = TextureBleedSpecs.DEFAULT_PADDING;
 	}
 
-	private void process(File png, TextureBleedResultImpl result) throws IOException {
-		FileResultImpl fileResult = new FileResultImpl();
-		fileResult.setProcessedFile(png);
-		long start_time = System.currentTimeMillis();
-		result.addFileResult(fileResult);
-		System.out.println("Processing: " + png);
-		BufferedImage image = ImageGWT.readFromFile(png);
-		EditableColorMap img = ImageGWT.newGWTColorMap(image);
-		W = img.getWidth();
-		H = img.getHeight();
+	System.out.println("maxScans: " + maxScans);
 
-		// Set<Integer> colors = JUtils.newSet();
-		HashSet<Int2> border = new HashSet<Int2>();
-
-		Color[][] function = new Color[W][H];
-
-		for (int x = 0; x < W; x++) {
-			for (int y = 0; y < H; y++) {
-				Int2 pointer = IntegerMath.newInt2(x, y);
-				Color color = img.getValue(x, y);
-				if (color.alpha() > 0.999f) {
-					function[x][y] = color;
-					// colors.add(0);
-				} else if (hasNonTransparentNeighbour(x, y, img)) {
-					border.add(pointer);
-
-				}
-
-			}
-		}
-		int k = 1;
-		long timer_start = Sys.SystemTime().currentTimeMillis();
-		long timer = 0;
-		long DELTA = 100;
-		for (; border.size() > 0; k++) {
-
-			if (k >= maxScans) {
-				break;
-			}
-			long current = Sys.SystemTime().currentTimeMillis();
-			long delta = current - timer_start;
-
-			timer_start = current;
-			timer = timer + delta;
-			while (timer > DELTA) {
-				System.out.print('.');
-				timer = timer - DELTA;
-			}
-			//
-			border = scan(function, k, img, border);
-		}
-		System.out.println();
-		// System.out.println("Scans performed: " + k);
-		fileResult.setScansPerformed(k);
-		// int maxDistance = colors.size();
-		for (int x = 0; x < W; x++) {
-			for (int y = 0; y < H; y++) {
-				Color colorValue = null;
-				colorValue = function[x][y];
-				if (colorValue == null) {
-					colorValue = Colors.PURPLE();
-				}
-				img.setValue(x, y, colorValue);
-			}
-		}
-		image = ImageGWT.toGWTImage(img);
-		ImageGWT.writeToFile(image, png, "png");
-		long mills = System.currentTimeMillis() - start_time;
-		fileResult.setDoneInMills(mills);
+	for (File png : pngFiles) {
+	    process(png, result);
 	}
 
-	private boolean hasNonTransparentNeighbour(int x0, int y0, EditableColorMap img) {
-		// TODO Auto-generated method stub
-		int D = 1;
-		for (int k = -D; k <= D; k++) {
-			for (int p = -D; p <= D; p++) {
-				if (k == 0 && p == 0) {
-					continue;
-				}
-				int x = (int) (k + x0);
-				int y = (int) (p + y0);
-				if (x < 0) {
-					continue;
-				}
-				if (y < 0) {
-					continue;
-				}
-				if (x >= W) {
-					continue;
-				}
-				if (y >= H) {
-					continue;
-				}
+	return result;
 
-				Color neighbour = img.getValue(x, y);
-				if (neighbour.alpha() > 0.999f) {
-					return true;
-				}
-			}
+    }
+
+    private void process(File png, TextureBleedResultImpl result) throws IOException {
+	FileResultImpl fileResult = new FileResultImpl();
+	fileResult.setProcessedFile(png);
+	long start_time = System.currentTimeMillis();
+	result.addFileResult(fileResult);
+	System.out.println("Processing: " + png);
+	BufferedImage image = ImageAWT.readFromFile(png);
+	EditableColorMap img = ImageAWT.newAWTColorMap(image);
+	W = img.getWidth();
+	H = img.getHeight();
+
+	// Set<Integer> colors = JUtils.newSet();
+	HashSet<Int2> border = new HashSet<Int2>();
+
+	Color[][] function = new Color[W][H];
+
+	for (int x = 0; x < W; x++) {
+	    for (int y = 0; y < H; y++) {
+		Int2 pointer = IntegerMath.newInt2(x, y);
+		Color color = img.valueAt(x, y);
+		if (color.alpha() > 0.999f) {
+		    function[x][y] = color;
+		    // colors.add(0);
+		} else if (hasNonTransparentNeighbour(x, y, img)) {
+		    border.add(pointer);
+
 		}
-		return false;
+
+	    }
 	}
+	int k = 1;
+	long timer_start = Sys.SystemTime().currentTimeMillis();
+	long timer = 0;
+	long DELTA = 100;
+	for (; border.size() > 0; k++) {
 
-	private HashSet<Int2> scan(Color[][] function, Integer borderIndex, EditableColorMap img, HashSet<Int2> border) {
-		HashSet<Int2> newBorder = new HashSet<Int2>();
-		for (Int2 pointer : border) {
-			int x = (int) pointer.getX();
-			int y = (int) pointer.getY();
-			if (function[x][y] != null) {
-				continue;
-			}
-			Color bestColor = addNUllNeighbours(x, y, newBorder, border, function);
-			function[x][y] = bestColor;
-		}
-		newBorder.removeAll(border);
-		return newBorder;
+	    if (k >= maxScans) {
+		break;
+	    }
+	    long current = Sys.SystemTime().currentTimeMillis();
+	    long delta = current - timer_start;
+
+	    timer_start = current;
+	    timer = timer + delta;
+	    while (timer > DELTA) {
+		System.out.print('.');
+		timer = timer - DELTA;
+	    }
+	    //
+	    border = scan(function, k, img, border);
 	}
-
-	Color addNUllNeighbours(int x0, int y0, HashSet<Int2> newBorder, HashSet<Int2> border, Color[][] function) {
-		// TODO Auto-generated method stub
-		HashSet<Int2> coloredNeighbours = new HashSet<Int2>();
-		int D = 1;
-		for (int k = -D; k <= D; k++) {
-			for (int p = -D; p <= D; p++) {
-				if (k == 0 && p == 0) {
-					continue;
-				}
-				int x = (int) (k + x0);
-				int y = (int) (p + y0);
-				if (x < 0) {
-					continue;
-				}
-				if (y < 0) {
-					continue;
-				}
-				if (x >= W) {
-					continue;
-				}
-				if (y >= H) {
-					continue;
-				}
-
-				Color neighbour = function[x][y];
-				Int2 pointer = IntegerMath.newInt2(x, y);
-				if (neighbour == null) {
-					newBorder.add(pointer);
-				} else if (!border.contains(pointer)) {
-					coloredNeighbours.add(pointer);
-
-				}
-			}
+	System.out.println();
+	// System.out.println("Scans performed: " + k);
+	fileResult.setScansPerformed(k);
+	// int maxDistance = colors.size();
+	for (int x = 0; x < W; x++) {
+	    for (int y = 0; y < H; y++) {
+		Color colorValue = null;
+		colorValue = function[x][y];
+		if (colorValue == null) {
+		    colorValue = Colors.PURPLE();
 		}
-		float r = 0;
-		float g = 0;
-		float b = 0;
-		for (Int2 neighbour : coloredNeighbours) {
-			Color color = function[(int) neighbour.getX()][(int) neighbour.getY()];
-			r = r + color.red();
-			g = g + color.green();
-			b = b + color.blue();
-		}
-		r = r / coloredNeighbours.size();
-		g = g / coloredNeighbours.size();
-		b = b / coloredNeighbours.size();
-
-		return Colors.newColor(ALPHA, r, g, b);
-
+		img.setValue(x, y, colorValue);
+	    }
 	}
+	image = ImageAWT.toAWTImage(img);
+	ImageAWT.writeToFile(image, png, "png");
+	long mills = System.currentTimeMillis() - start_time;
+	fileResult.setDoneInMills(mills);
+    }
 
-	float NORMAL_MODE_ALPHA = 0f;
-	float DEBUG_MODE_ALPHA = 1f;
-	float ALPHA = NORMAL_MODE_ALPHA;
+    private boolean hasNonTransparentNeighbour(int x0, int y0, EditableColorMap img) {
+	// TODO Auto-generated method stub
+	int D = 1;
+	for (int k = -D; k <= D; k++) {
+	    for (int p = -D; p <= D; p++) {
+		if (k == 0 && p == 0) {
+		    continue;
+		}
+		int x = (int) (k + x0);
+		int y = (int) (p + y0);
+		if (x < 0) {
+		    continue;
+		}
+		if (y < 0) {
+		    continue;
+		}
+		if (x >= W) {
+		    continue;
+		}
+		if (y >= H) {
+		    continue;
+		}
+
+		Color neighbour = img.valueAt(x, y);
+		if (neighbour.alpha() > 0.999f) {
+		    return true;
+		}
+	    }
+	}
+	return false;
+    }
+
+    private HashSet<Int2> scan(Color[][] function, Integer borderIndex, EditableColorMap img, HashSet<Int2> border) {
+	HashSet<Int2> newBorder = new HashSet<Int2>();
+	for (Int2 pointer : border) {
+	    int x = (int) pointer.getX();
+	    int y = (int) pointer.getY();
+	    if (function[x][y] != null) {
+		continue;
+	    }
+	    Color bestColor = addNUllNeighbours(x, y, newBorder, border, function);
+	    function[x][y] = bestColor;
+	}
+	newBorder.removeAll(border);
+	return newBorder;
+    }
+
+    Color addNUllNeighbours(int x0, int y0, HashSet<Int2> newBorder, HashSet<Int2> border, Color[][] function) {
+	// TODO Auto-generated method stub
+	HashSet<Int2> coloredNeighbours = new HashSet<Int2>();
+	int D = 1;
+	for (int k = -D; k <= D; k++) {
+	    for (int p = -D; p <= D; p++) {
+		if (k == 0 && p == 0) {
+		    continue;
+		}
+		int x = (int) (k + x0);
+		int y = (int) (p + y0);
+		if (x < 0) {
+		    continue;
+		}
+		if (y < 0) {
+		    continue;
+		}
+		if (x >= W) {
+		    continue;
+		}
+		if (y >= H) {
+		    continue;
+		}
+
+		Color neighbour = function[x][y];
+		Int2 pointer = IntegerMath.newInt2(x, y);
+		if (neighbour == null) {
+		    newBorder.add(pointer);
+		} else if (!border.contains(pointer)) {
+		    coloredNeighbours.add(pointer);
+
+		}
+	    }
+	}
+	float r = 0;
+	float g = 0;
+	float b = 0;
+	for (Int2 neighbour : coloredNeighbours) {
+	    Color color = function[(int) neighbour.getX()][(int) neighbour.getY()];
+	    r = r + color.red();
+	    g = g + color.green();
+	    b = b + color.blue();
+	}
+	r = r / coloredNeighbours.size();
+	g = g / coloredNeighbours.size();
+	b = b / coloredNeighbours.size();
+
+	return Colors.newColor(ALPHA, r, g, b);
+
+    }
+
+    float NORMAL_MODE_ALPHA = 0f;
+    float DEBUG_MODE_ALPHA = 1f;
+    float ALPHA = NORMAL_MODE_ALPHA;
 
 }
